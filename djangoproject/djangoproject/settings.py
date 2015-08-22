d28ccccff7b1f37d3eea2c78f03b8e74edc679fc
Tarun Mukherjee <tmukherjee13@gmail.com>
2015-08-22 23:53:13 +0530

    created app

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -75,9 +75,15 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_angular',
+        'USER': 'root',
+        'PASSWORD': 'Passw0rd',
     }
 }
 
